f4b39c3c818f46d83d72dd7466311b237be4a78a
Evan Bacon <baconbrix@gmail.com>
2019-11-20 14:03:55 -0800

    back -> hooks

--- components/Button/Back.js
+++ components/Button/Back.js
@@ -1,32 +1,23 @@
-import React, { Component } from 'react';
-import {
-  Image,
-  StyleSheet,
-  TouchableOpacity as TouchableBounce,
-} from 'react-native';
+import * as React from 'react';
+import { Image, StyleSheet, TouchableOpacity as TouchableBounce } from 'react-native';
 
 import Images from '../../src/Images';
 
-export default class Button extends Component {
-  render() {
-    return (
-      <TouchableBounce
-        onPress={this.props.onPress}
-        style={[styles.container, this.props.style]}
-      >
-        <Image
-          source={Images.button.back}
-          style={[styles.image, this.props.imageStyle]}
-        />
-      </TouchableBounce>
-    );
-  }
+export default function Button({ onPress, style, imageStyle }) {
+  return (
+    <TouchableBounce
+      onPress={onPress}
+      style={style}
+    >
+      <Image
+        source={Images.button.back}
+        style={[styles.image, imageStyle]}
+      />
+    </TouchableBounce>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-  },
   image: {
     resizeMode: 'contain',
     height: 48,
