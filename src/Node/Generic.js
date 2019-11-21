a0dc88648a9036bea673fcbb781d04ee404704fd
Evan Bacon <baconbrix@gmail.com>
2019-11-20 17:08:06 -0800

    Update Generic.js

--- src/Node/Generic.js
+++ src/Node/Generic.js
@@ -3,37 +3,49 @@ import { loadObjAsync, THREE } from 'expo-three';
 import Models from '../../src/Models';
 import CrossyMaterial from '../CrossyMaterial';
 
+function setShadows(mesh, { castShadow, receiveShadow }) {
+  mesh.traverse(child => {
+    if (child instanceof THREE.Mesh) {
+      child.castShadow = castShadow;
+      child.receiveShadow = receiveShadow;
+    }
+  });
+
+  mesh.castShadow = castShadow;
+  mesh.receiveShadow = receiveShadow;
+}
 export default class Generic {
   models = {};
 
   globalModels = Models;
 
+  getSize = mesh => {
+    let box = new THREE.Box3().setFromObject(mesh);
+    // console.log( box.min, box.max, box.size() );
+    return box.size();
+  };
   getWidth = mesh => {
-    var box = new THREE.Box3().setFromObject(mesh);
+    let box = new THREE.Box3().setFromObject(mesh);
     // console.log( box.min, box.max, box.size() );
     return box.size().width;
   };
 
-  _downloadAssets = async ({ model, texture, castShadow, receiveShadow }) => {
-    const _model = await loadObjAsync({ asset: model });
+  _downloadAssets = async ({ name, model, texture, castShadow, receiveShadow }) => {
     const material = CrossyMaterial.load(texture)
+    const _model = await loadObjAsync({ asset: model });
 
     _model.traverse(child => {
       if (child instanceof THREE.Mesh) {
         child.material = material;
-        child.castShadow = castShadow;
-        child.receiveShadow = receiveShadow;
       }
     });
 
-    _model.castShadow = castShadow;
-    _model.receiveShadow = receiveShadow;
-
+    setShadows(_model, { castShadow, receiveShadow })
     return _model;
   };
 
   getRandom = () => {
-    var keys = Object.keys(this.models);
+    let keys = Object.keys(this.models);
     return this.models[keys[(keys.length * Math.random()) << 0]].clone();
   };
 
