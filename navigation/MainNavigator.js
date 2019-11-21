635481a47ec6fa720acf54e8b7e01e2bafe9d1f9
Evan Bacon <baconbrix@gmail.com>
2019-11-20 17:24:39 -0800

    use react-navigation-stack

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,4 +1,4 @@
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 // import Home from '../screens/HomeScreen';
 import Game from '../screens/GameScreen';
