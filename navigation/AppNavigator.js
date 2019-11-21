e6e967b6cf9fd68aaf2be3ea935f48e153d5ecd6
Evan Bacon <baconbrix@gmail.com>
2019-11-20 17:24:43 -0800

    Update AppNavigator.js

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,13 +1,4 @@
-import { createBrowserApp } from '@react-navigation/web';
-import { Platform } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-
 import createApp from './createApp';
 import MainNavigator from './MainNavigator';
-
-const createApp = Platform.select({
-    web: createBrowserApp,
-    default: createAppContainer,
-})
 
 export default createApp(MainNavigator);
