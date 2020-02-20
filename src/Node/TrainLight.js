d0f2125e4ae184414b7f525a9e1af5dd84a3e5ee
Evan Bacon <baconbrix@gmail.com>
2020-02-20 10:03:13 -0800

    Update TrainLight.js

--- src/Node/TrainLight.js
+++ src/Node/TrainLight.js
@@ -8,11 +8,10 @@ export default class TrainLight extends Generic {
       },
     } = this.globalModels;
 
-    const model = await this._download(inactive);
-    this.models[`${0}`] = model;
+    await this._register(`0`, inactive);
 
     for (let i = 0; i < 2; i++) {
-      this.models[`active_${i}`] = await this._download(active[`${i}`]);
+      await this._register(`active_${i}`, active[`${i}`]);
     }
     return this.models;
   };
