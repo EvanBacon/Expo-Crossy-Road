a6b8212405c7b114494c30efe687da2a7e3e8cde
Evan Bacon <baconbrix@gmail.com>
2020-02-20 10:03:19 -0800

    Update River.js

--- src/Node/River.js
+++ src/Node/River.js
@@ -5,8 +5,7 @@ export default class River extends Generic {
     const {
       environment: { river },
     } = this.globalModels;
-    let model = await this._download({ ...river, castShadow: false, receiveShadow: true });
-    this.models[`${0}`] = model;
-    return model;
+    await this._register(`0`, { ...river, castShadow: false, receiveShadow: true });
+    return this.models;
   };
 }
