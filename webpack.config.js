6d6862357500aec6af5a757d641d4169bcd7a4da
evanbacon <bacon@expo.io>
2022-03-22 19:48:49 -0500

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config');
-
-module.exports = async function (env, argv) {
-  const isProd = env.mode === 'production';
-  const config = await createExpoWebpackConfigAsync({ ...env, removeUnusedImportExports: isProd, report: isProd }, argv);
-  // Customize the config before returning it.
-  return config;
-};
