b5cf73c4fd0b173c00064824e95f299c5d445a99
evanbacon <bacon@expo.io>
2025-12-28 11:55:50 -0600

    make chicken the default

--- src/Characters.ts
+++ src/Characters.ts
@@ -3,28 +3,28 @@ export default {
     name: "Chicken",
     id: "chicken",
   },
-  bacon: {
-    name: "Bacon",
-    id: "bacon",
-  },
-  brent: {
-    name: "Brent Vatne",
-    id: "brent",
-  },
-  avocoder: {
-    name: "Avocoder",
-    id: "avocoder",
-  },
-  wheeler: {
-    name: "Wheeler",
-    id: "wheeler",
-  },
-  palmer: {
-    name: "Palmer",
-    id: "palmer",
-  },
-  juwan: {
-    name: "Juwan",
-    id: "juwan",
-  },
+  // bacon: {
+  //   name: "Bacon",
+  //   id: "bacon",
+  // },
+  // brent: {
+  //   name: "Brent Vatne",
+  //   id: "brent",
+  // },
+  // avocoder: {
+  //   name: "Avocoder",
+  //   id: "avocoder",
+  // },
+  // wheeler: {
+  //   name: "Wheeler",
+  //   id: "wheeler",
+  // },
+  // palmer: {
+  //   name: "Palmer",
+  //   id: "palmer",
+  // },
+  // juwan: {
+  //   name: "Juwan",
+  //   id: "juwan",
+  // },
 };
